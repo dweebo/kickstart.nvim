e9f83f9b154b38b0b6d1b99ebe2c31a932554ba0
Peter Hewitt <peter.hewitt@upstart.com>
2023-04-13 09:49:46 -0400

    setup my existing bindings for search files, search grep to use telescope instead

--- init.lua
+++ init.lua
@@ -288,6 +288,11 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
+-- peter add
+vim.keymap.set('n', '<leader>t', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>a', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+-- end peter add
+
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
